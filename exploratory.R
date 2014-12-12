bfb545c2de7a1d611f320c0ab4845974708a0cfd
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-12-11 18:34:07 -0800

    Model building

--- exploratory.R
+++ exploratory.R
@@ -90,8 +90,49 @@ summary(mod1)
 
 library(leaps)
 
-r1 = regsubsets(exchange_rate ~ ., data=trainset)
-s1 = summary(r1)
+rs1 = regsubsets(exchange_rate ~ ., data=trainset, nbest=1, nvmax=21)
+rs1s = summary(rs1)
+
+modelstats = data.frame(p = 2:21,
+    sse = rs1s$rss,
+    r2 = rs1s$rsq,
+    r2a = rs1s$adjr2,
+    cp = rs1s$cp)
+
+modelstats
+
+
+fullmod = mod1
+fullMSE = summary(fullmod)$sigma ** 2
+
+ssto = sum((trainset$exchange_rate - mean(trainset$exchange_rate)) ** 2)
+
+n = nrow(trainset)
+
+############################################################
+# SSE should be decreasing and nonnegative
+# Very surprisingly, rss here is what we have been calling sse!
+modelstats = data.frame(p = 1:ncol(trainset) + 1,
+    sse = rs1s$rss,
+    r2 = rs1s$rsq,
+    r2a = rs1s$adjr2,
+    cp = rs1s$cp)
+
+# define the first row
+row1 = data.frame(p = 1, sse = ssto, r2 = 0, r2a = 0,
+                  cp = ssto / fullMSE - (n - 2))
+
+# Add the first row with the others
+modelstats = rbind(row1, modelstats)
+
+# Add AIC, BIC columns
+modelstats$aic = with(modelstats, n * log(sse / n) + 2 * p)
+modelstats$bic = with(modelstats, n * log(sse / n) + log(n) * p)
+
+modelstats
+sapply(modelstats, which.min)
+#
+
 
 showmodel = function(p, leapsummary){
     # returns the coefficient names from the model with p coefficients
@@ -103,6 +144,128 @@ showmodel = function(p, leapsummary){
 # Only picked 9- and they look like the ones that were more of a cloud from
 # the EDA- the nopattern ones
 nopattern
-sapply(1:9, showmodel, s1)
+sapply(1:9, showmodel, rs1s)
 
+hist(country$inflation_USA)
+
+# Try adding KOR / USD variables
+ratio = with(country, 
+             data.frame(exchange_rate = exchange_rate
+                        , gdp = gdp_KOR / gdp_USA
+                        , unemployment = unemployment_KOR / unemployment_USA
+                        , debt = debt_KOR / debt_USA
+                        , imports = imports_KOR / imports_USA
+                        , exports = exports_KOR / exports_USA
+                        , interest_rate = interest_rate_KOR / interest_rate_USA
+                        , inflation_KOR = inflation_KOR
+                        , inflation_USA = inflation_USA
+                        , cpi = cpi_KOR / cpi_USA
+                        , govspending = govspending_KOR / govspending_USA
+                        ))
+
+fitratio = lm(exchange_rate ~ ., data=ratio)
+summary(fitratio)
+
+# Another test train split
+set.seed(978)
+a = ttsplit(ratio, 1/3)
+tr = a[[1]]
+test = a[[2]]
+dim(tr)
+head(tr)
+
+library(MASS)
+
+fit3 = lm(exchange_rate ~ ., data=tr)
+boxcox(fit3)
+
+# Try an inverse square transformation
+fit4 = lm(I(1 / exchange_rate^2) ~ ., data=tr)
+summary(fit4)
+plot(fit4, which=1)
+
+# Not plotting QQ plot
+qqnorm(fit4$residuals)
+qqline(fit4$residuals)
+
+
+# Try an inverse transformation
+# We like this one better :)
+fit5 = lm(I(1 / exchange_rate) ~ ., data=tr)
+summary(fit5)
+plot(fit5, which=1)
+
+# Not plotting QQ plot
+qqnorm(fit5$residuals)
+
+library(leaps)
+
+fit6 = regsubsets(I(1 / exchange_rate) ~ ., data=tr, nvmax=11)
+#fit6 = regsubsets(, data=trainset, nbest=1, nvmax=21)
+sfit6 = summary(fit6)
+
+modelstats = data.frame(
+    sse = sfit6$rss,
+    bic = sfit6$bic,
+    r2 = sfit6$rsq,
+    r2a = sfit6$adjr2,
+    cp = sfit6$cp)
+
+modelstats
+
+finalvars = sfit6$which[5, ]
+
+tdata = tr[, sfit6$which[5, ]]
+dim(tdata)
+names(tdata)
+
+fit7 = lm(I(1 / exchange_rate) ~ ., data=tdata)
+summary(fit7)
+
+qqnorm(fit7$residuals)
+# This looks more reasonable
+pdf('cooks.pdf')
+plot(fit7, which=4)
+dev.off()
+# 
+
+infm = influence.measures(fit7)
+summary(infm)
+
+# DFFIT is a little large. Shouldn't be bigger than this:
+2 * sqrt(6 / 121)
+# 113 and 117 are our big ones
+# What happened then?
+country[c(113, 117), 'Date']
+# Prime mortgage rate crash!!
+
+# How is the fit when we remove the two outliers?
+outindex = which(row.names(tdata) %in% c('112', '113', '117'))
+#outindex = which(row.names(tdata) == '113')
+tdata8 = tdata[-outindex, ]
+
+finalform = I(1 / exchange_rate) ~ .
+
+fit8 = lm(I(1 / exchange_rate) ~ ., data=tdata8)
+summary(fit8)
+
+plot(fit8, which=4)
+qqnorm(fit8$residuals)
+
+# This is looking much better.
+# Try it on the test set
+test9 = test[, finalvars]
+fit9 = lm(I(1 / exchange_rate) ~ ., data=test9)
+summary(fit9)
+plot.lm(fit9, which=4)
+
+coef(fit8)
+coef(fit9)
+
+form10 = exchange_rate ~ gdp + interest_rate + inflation_KOR + inflation_USA
+fit10 = lm(form10, data= tdata8)
+# Do we still need to do inverse transform if outliers are gone? 
+boxcox(fit10)
+# Still calls for the inverse transform even with the outliers removed.
+summary(fit10)
 
